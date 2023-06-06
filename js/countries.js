19455fd637fd8abaae2873290128ccc050a8c3e3
hasibprangon <hasibulhossainp09@gmail.com>
2023-06-06 20:14:36 +0600

    dynamically create HTML element

--- js/countries.js
+++ js/countries.js
@@ -5,7 +5,22 @@ const loadCountries = () => {
 }
 
 const displayCountries = countries =>{
-console.log(countries);
+console.log(countries[0]);
+const allCountriesHTML = countries.map (country =>getCountryHTML(country))
+// console.log(allCountriesHTML[0]);
+const container = document.getElementById('countries');
+container.innerHTML = allCountriesHTML.join(' ');
 }
+
+const getCountryHTML = country =>{
+    return `
+    <div class="country">
+    <img src="${country.flags.png}">
+    <h2>${country.name.common}</h2>
+    </div>
+
+    `
+}
+
 
 loadCountries()
